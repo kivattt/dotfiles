f7c7bec05208f6eec93abee6028cfb9f67cb11fd
kivattt <kivattt@gmail.com>
2024-02-22 12:14:15 +0100

    Add some files to diff_with_local.sh, update Neovim config

---
+++ diff_with_local.sh
@@ -0,0 +1,7 @@
+git diff .bashrc ~/.bashrc
+git diff .tmux.conf ~/.tmux.conf
+git diff .vimrc ~/.vimrc
+git diff .Xresources ~/.Xresources
+
+git diff .config/i3/config ~/.config/i3/config
+git diff .config/nvim/init.lua ~/.config/nvim/init.lua
