42a3f3001510559290181e6ba3af6eb73f048552
kivattt <kivattt@gmail.com>
2024-03-06 06:31:06 +0100

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,37 @@
+--[[
+	PLUGINS (vim-plug)
+]]--
+
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin")
+
+-- LSP Support
+Plug("neovim/nvim-lspconfig")
+-- Autocompletion
+Plug("hrsh7th/nvim-cmp")
+Plug("hrsh7th/cmp-nvim-lsp")
+Plug("L3MON4D3/LuaSnip")
+
+Plug("VonHeikemen/lsp-zero.nvim", {["branch"] = "v3.x"})
+
+-- Mason
+Plug("williamboman/mason.nvim")
+
+-- Mason-lspconfig
+Plug("williamboman/mason-lspconfig.nvim")
+
+-- Telescope
+-- XXX: Telescope live_grep requires "ripgrep" to be installed
+-- https://github.com/BurntSushi/ripgrep
+Plug("nvim-lua/plenary.nvim")
+Plug("nvim-telescope/telescope.nvim", { ["branch"] = "0.1.x" })
+
+vim.call("plug#end")
+
+--[[
+	OPTIONS
+]]--
+
 --vim.cmd "colorscheme desert"
 vim.cmd "colorscheme industry"
 
@@ -12,6 +46,10 @@ vim.opt.title = true
 vim.cmd "set titlestring=%Y\\ %t%(\\ %M%)%(\\ (%{expand(\\\"%:~:.:h\\\")})%)%(\\ %a%)"
 
 local map = vim.keymap.set
+
+--[[
+	KEYMAPS
+]]--
 
 -- Insert mode maps
 map("i", "<C-Tab>", "<Esc>gti")
@@ -41,39 +79,41 @@ map("n", "<C-k>", vim.diagnostic.goto_prev)
 
 map("n", "?", vim.lsp.buf.hover)
 
--- Plugins (vim-plug)
-local Plug = vim.fn["plug#"]
-vim.call("plug#begin")
+vim.g.mapleader = ","
 
--- LSP Support
-Plug("neovim/nvim-lspconfig")
--- Autocompletion
-Plug("hrsh7th/nvim-cmp")
-Plug("hrsh7th/cmp-nvim-lsp")
-Plug("L3MON4D3/LuaSnip")
+--[[
+	PLUGIN stuff
+]]--
+local builtin = require("telescope.builtin")
+map("n", "<leader>f", builtin.find_files, {})
+map("n", "<leader>g", builtin.live_grep, {})
+map("n", "<leader>b", builtin.buffers, {})
+map("n", "<leader>h", builtin.help_tags, {})
 
-Plug("VonHeikemen/lsp-zero.nvim", {["branch"] = "v3.x"})
-
--- Mason
-Plug("williamboman/mason.nvim")
-
--- Mason-lspconfig
-Plug("williamboman/mason-lspconfig.nvim")
-
-vim.call("plug#end")
+require("telescope").setup({
+	defaults = {
+		layout_config = {
+			horizontal = {width = 0.99}
+		}
+	}
+})
 
 local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(client, bufnr)
 		lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require("mason").setup()
+require("mason").setup({})
 
+--[[
+	LSP SETUP
+]]--
 local lspconfig = require("lspconfig")
-
-lspconfig.gopls.setup({})
-lspconfig.clangd.setup({})
-lspconfig.ocamllsp.setup({})
+lspconfig.gopls.setup({})    -- Go
+lspconfig.clangd.setup({})   -- C/C++
+lspconfig.ocamllsp.setup({}) -- OCaml
+lspconfig.lua_ls.setup({})   -- Lua
+lspconfig.jdtls.setup({})    -- Java (Requires Java 17+ to be used, tldr: `sudo update-alternatives --config java`)
 
 -- Tab for autocomplete key map
 local cmp = require("cmp")
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ## My configuration files
 `autoexec.cfg` is my config file for Team Fortress 2
+
+To use the JetBrains Mono font with XTerm, run this:
+```console
+cd ~/Downloads
+mkdir JetBrainsMono
+cd JetBrainsMono
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+unzip JetBrainsMono-2.304.zip
+mv fonts/ttf fonts/JetBrainsMono
+sudo cp -r fonts/JetBrainsMono /usr/share/fonts/truetype
+```
