2fa35ef4fa76e8a8859076cb54d337948b844479
kivattt <kivattt@gmail.com>
2024-04-03 07:52:40 +0200

    Make diff_with_local.sh more generic

--- diff_with_local.sh
+++ diff_with_local.sh
@@ -1,16 +1,7 @@
-git diff .bashrc ~/.bashrc
-git diff .tmux.conf ~/.tmux.conf
-git diff .vimrc ~/.vimrc
-git diff .Xresources ~/.Xresources
-
-git diff .config/i3/config ~/.config/i3/config
-git diff .config/nvim/init.lua ~/.config/nvim/init.lua
-git diff .config/picom/picom.conf ~/.config/picom/picom.conf
-
-# .kiva files
-git diff .config/nvim/syntax/kiva.vim ~/.config/nvim/syntax/kiva.vim
-git diff .config/nvim/ftdetect/kiva.vim ~/.config/nvim/ftdetect/kiva.vim
-
-# .log files
-git diff .config/nvim/syntax/log.vim ~/.config/nvim/syntax/log.vim
-git diff .config/nvim/ftdetect/log.vim ~/.config/nvim/ftdetect/log.vim
+for f in $(find . -type f); do
+	noDotSlash=${f#"./"}
+	if [[ "$noDotSlash" =~ ".git" ]] || [[ "$noDotSlash" == "README.md" ]] || [[ "$noDotSlash" == "diff_with_local.sh" ]]; then
+		continue
+	fi
+	git diff "$noDotSlash" ~/"$noDotSlash" 2> /dev/null
+done
