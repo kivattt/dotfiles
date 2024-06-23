8d353d0552927d5b327adf38562f24ce613c4a13
kivattt <kivattt@gmail.com>
2024-06-23 03:43:37 +0200

    mp4.bash: Also check if ffprobe doesn't exist

--- .config/fen/mp4.bash
+++ .config/fen/mp4.bash
@@ -7,11 +7,19 @@ fi
 file -b "$1"
 
 if type jq > /dev/null 2> /dev/null; then
-	ffprobe -v quiet -print_format json -show_format -show_streams "$1" \
-		| jq -r '.streams[] | "Dimensions: \(.width) x \(.height)"' 2> /dev/null \
-		| grep -v "null x null"
+	printf "" # No-op
 else
 	echo "jq missing, it is required to show video dimensions"
 	exit 2
 fi
 
+if type ffprobe > /dev/null 2> /dev/null; then
+	printf "" # No-op
+else
+	echo "ffprobe missing, it is required to show video dimensions"
+	exit 3
+fi
+
+ffprobe -v quiet -print_format json -show_format -show_streams "$1" \
+	| jq -r '.streams[] | "Dimensions: \(.width) x \(.height)"' 2> /dev/null \
+	| grep -v "null x null"
