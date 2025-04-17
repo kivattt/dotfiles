61a9e7ae7414f04375cfcc3b6b1a458372a7a453
kivattt <kivattt@gmail.com>
2025-04-17 22:40:40 +0200

    Update TF2 and CS2 and diff_with_local.sh

--- diff_with_local.sh
+++ diff_with_local.sh
@@ -3,5 +3,22 @@ for f in $(find . -type f); do
 	if [[ "$noDotSlash" =~ ".git" ]] || [[ "$noDotSlash" == "README.md" ]] || [[ "$noDotSlash" == "diff_with_local.sh" ]]; then
 		continue
 	fi
+
 	git diff "$noDotSlash" ~/"$noDotSlash" 2> /dev/null
 done
+
+cs2autoexec=~/".steam/debian-installation/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg/autoexec.cfg"
+if test -f "$cs2autoexec"; then
+	git diff cs2-config/autoexec.cfg "$cs2autoexec"
+else
+	echo "WARNING: Missing CS2 autoexec.cfg file at:"
+	echo "$cs2autoexec"
+fi
+
+tf2autoexec=~/".steam/debian-installation/steamapps/common/Team Fortress 2/tf/cfg/autoexec.cfg"
+if test -f "$tf2autoexec"; then
+	git diff tf2-config/autoexec.cfg "$tf2autoexec"
+else
+	echo "WARNING: Missing TF2 autoexec.cfg file at:"
+	echo "$cs2autoexec"
+fi
